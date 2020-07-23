9e7872eb181aaea368de7d179aa8c236e7767c1a
Irene <airine@Irenes-MacBook-Air.local>
2020-07-23 14:55:05 -0700

    [constructor] Add Car function

--- app.js
+++ app.js
@@ -42,3 +42,43 @@ carTwo.seats = 4;
 console.log(carTwo.seats)  //inherit
 
 
+// Constructor Functions, need to capatalize "U"
+function User(name){
+    this.name = name;   //referring to the User //no need to return a function bcos it's constructor function
+
+} 
+
+let adam = new User('Adam');
+let pete = new User('Pete');
+console.log(adam.name);   //
+console.log(pete.name);
+
+function NBAPlayer(name, team, threePointShooters) {
+    // this = {}
+    this.name = name;
+    this.team = team;
+    this.threePointShooters = threePointShooters;
+
+    // return this
+}
+let steph = new NBAPlayer('Steph Curry', 'Warriors', true);
+let lebron = 
+console.log(steph);
+console.log(steph.name);
+
+// make a constructor function
+// Make 3 new variables with that constructor function
+
+function Car(name, year, originOf) {
+    // this = {}
+    this.name = name;
+    this.year = year;
+    this.originOf = originOf;
+
+    // return this
+}
+let camry = new Car('toyota', 2020, 'Japan');
+let crv =  new Car('honda', 2019, 'Japan');
+console.log(camry);
+console.log(camry.name);
+console.log(crv)
